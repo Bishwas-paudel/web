25ba99e172ff2c292ddc637b882d13a1aeba05d2
Bishwas-paudel <paudelbishwas885@gmail.com>
2023-12-11 10:29:54 +0545

    js first day

---
+++ day12.js
@@ -0,0 +1,23 @@
+// scripting lanuage -client side scripting dynamic computer programming language it is also known as live
+//  sccripting language       used as a part of web pages
+// behaviour language  load after 10 second , open and platform independent
+//   assignment 2--difference between client side scripting and server side scripting
+//1. client side scripting: client side ma execute hunxa ,fast secure and easy to use  browser control dynamic look
+//   client side scripting is used to interact with the user and the browser
+//   server side scripting is used to interact with the server and the database
+//advantage of JS
+// less server interaction
+// immediate feedback to the visiter
+// increase interactivity-movement images
+// richer interfaces
+//disadvantage
+//doesnt allow the reading and writing files 
+//cant use in networking application
+//not have multitheading and multiprocessig
+//syntax:
+// can be used as both internal and external and inline
+//<script>mistake gare error aaucha  </script>
+//document.write ("hello world")
+//<script language ="javascript" type ="text/js"
+
+
