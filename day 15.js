4aee30307aa47d830ab5438cb2f07a3104f72e6e
Bishwas-paudel <paudelbishwas885@gmail.com>
2023-12-18 22:18:24 +0545

    js loops

--- day 15.js
+++ day 15.js
@@ -2,7 +2,7 @@
 function loop() {
  let hello = "";
   for (let i = 1; i < 1000; i++) {
- hello += "bishwas"
+ hello += "bishwas<br>"
   }
  
  document.getElementById("test").innerHTML = hello;
---
+++ day17.js
@@ -0,0 +1,8 @@
+function f1(){
+// let text="";
+// let i=0;
+// while(i<10){
+//  text +="<br>the number is &nbsp"+ i;
+//  i++;
+// }
+// document.getElementById("demo").innerHTML =text;}
