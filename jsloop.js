d7623c854852a33991de618aad9becc15f740180
Bishwas-paudel <paudelbishwas885@gmail.com>
2023-12-19 21:31:14 +0545

    jsloopp

---
+++ jsloop.js
@@ -0,0 +1,6 @@
+// jsloop.js content
+let e = "";
+for (let i = 0; i < 10; i++) {
+    e += i + "<br>"; 
+}
+document.getElementById("DEMO").innerHTML = e; // Setting the inner HTML of the element with ID "DEMO"
